7852c556f5fed3d6aa9b26e293b0b894a17b5582
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-02-17 11:48:00 +0700

    ...

---
+++ Buoi4/HomeWork/Bai2/Form8.js
@@ -0,0 +1,8 @@
+
+function send() {
+    document.getElementById("getAnswer").innerHTML = "Đã gửi thông tin";
+}
+
+ function abort() {
+    document.getElementById("getAnswer").innerHTML = "Đã xóa thông tin";
+}
